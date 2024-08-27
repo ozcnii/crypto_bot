730c9227cee82a875a860bcb6c4f466bee5c46b7
Andrets <dmitriysurkov2019@gmail.com>
2024-08-27 19:29:12 +0500

    fix paths in deployment

--- CryptoAPI/CryptoAPI/alembic/env.py
+++ CryptoAPI/CryptoAPI/alembic/env.py
@@ -4,7 +4,7 @@ from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-import Database
+from CryptoAPI.Database import DataBase
 from alembic import context
 from good_guard import DATABASE_URL
 
@@ -25,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Database.DataBase.metadata
+target_metadata = DataBase.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
