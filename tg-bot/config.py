81a8011ee906d932db37fd294ab67c031b88b2d0
ozcnii <ercount89@gmail.com>
2025-01-13 22:53:51 +0400

    fix(bot): backend host url

--- tg-bot/config.py
+++ tg-bot/config.py
@@ -5,7 +5,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Отключение предупреждений о модификациях
 
 class DevelopmentConfig(Config):
-    HOST = 'http://127.0.0.1:5000'
+    HOST = 'http://backend:5000'
     DEBUG = True
     BOTLINK = getenv('BOT_LINK')
     TOKEN=getenv('TELEGRAM_API_TOKEN')
